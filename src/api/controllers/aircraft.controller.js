d592a9199fb0a8a816ad49eba4d2bcc22dc9d3ee
matiasolocco <matias.olocco@hotmail.com>
2024-04-28 21:31:18 +0200

    modelos aircrafts iniciados

--- src/api/controllers/aircraft.controller.js
+++ src/api/controllers/aircraft.controller.js
@@ -12,4 +12,39 @@ const addAircraft = async (req, res) => {
   }
 };
 
-module.exports = { addAircraft };
+
+const selectAircraft = async (req, res) => {
+  const airlines = await Airline.find();
+  return res.status(200).json(airlines)
+}
+const selectOnePet = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const findPet = await Pet.findOne({ _id: id })
+      return res.status(200).json(findPet)
+
+  } catch (error) {
+      console.log(error)
+      return res.status(500).json(error)
+  }
+}
+const updateAircraft = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const petBody = new Airline(req.body)
+      airlineBody._id = id;
+      const updateAirline = await Airline.findByIdAndUpdate(id, airlineBody, { new: true })
+      console.log(updateAirline)
+      if (!updateAirline) {
+          return res.status(404).json({ message: "Airline no existe" })
+      }
+      return res.status(200).json(updateAirline)
+
+  } catch (error) {
+      console.log(error)
+      return res.status(500).json(error)
+  }
+
+}
+
+module.exports = {addAircraft, selectAircraft, updateAircraft, deleteAircraft};
