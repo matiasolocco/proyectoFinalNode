cf72744edfb15f16802b3fae1c716bb1908aa73f
matiasolocco <matias.olocco@hotmail.com>
2024-05-01 11:49:55 +0200

    test Postman user/register: SUCCES

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -5,6 +5,7 @@ const userSchema = new Schema(
   {
     name: { type: String, require: true },
     lastname: { type: String, require: true },
+    //Establezco la relacion entre user y los aviones (Aircraft) en los que ha volado
     aircraft: [{ type: Schema.ObjectId, ref: 'aircraft' }], 
     email: { type: String, require: true },
     password: { type: String, require: true },
