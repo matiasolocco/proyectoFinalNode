dde7da4d939b7927c541f76dc3804993d45489bc
matiasolocco <matias.olocco@hotmail.com>
2024-05-01 02:27:32 +0200

    user update

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -74,5 +74,41 @@ const modifyProfile = async (req, res) => {
   const updateUser = await User.findByIdAndUpdate(req.userProfile._id, newUser, { new: true })
   return res.status(200).json({ data: updateUser })
 }
+const addUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const createdUser = await newUser.save();
+    return res.json(createdUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
 
-module.exports = { register, login, modifyProfile };
+const updateUser = async (req, res) => {
+  try {
+    const idUser = req.params.id;
+    const idPet = req.body.id;
+    console.log(idUser, idPet);
+    const modifyUser = await User.findByIdAndUpdate(
+      idUser,
+      { $push: { pet: idPet } },
+      { new: true }
+    );
+    return res.status(200).json(modifyUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
+
+const selectUser = async (req, res) => {
+  const nameUser = req.query.name;
+  //find({ name: 'alfonsina' })
+  const users = await User.find({ name: nameUser }) //findOne devuelve el primero que encuentra
+    .populate({ path: 'pet', select: 'name' })
+    .populate({ path: 'doctor', select: 'name' });
+  return res.status(200).json(users);
+};
+
+module.exports = { register, login, modifyProfile, addUser, selectUser, updateUser };
