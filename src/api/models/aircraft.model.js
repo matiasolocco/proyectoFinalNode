4f41a1860510e00c8c417ccbdbbf0cd2670450d2
matiasolocco <matias.olocco@hotmail.com>
2024-05-01 12:07:51 +0200

    Create (CRUD) aircraft/add: SUCCES

--- src/api/models/aircraft.model.js
+++ src/api/models/aircraft.model.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const aircraftSchema = new Schema({
-  airline: { type: String, require: true },
   manufacturer: { type: String, require: true },
   model: { type: String, require: true },
   series: { type: String, require: true },
   year: { type: Number, require: true },
+  airline: { type: String, require: true },
   image: { type: String, default: "" }
 },
 {
